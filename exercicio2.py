0c36237a0a7834fa70c9c33a9f2688206fce8898
NicolasAskel <70597473+NicolasAskel@users.noreply.github.com>
2020-11-29 10:55:43 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+PrimeiroNumero = int(input("Insira o número para a multiplicação: "))
+SegundoNumero = int(input("Insira outro número para a multiplicação: "))
+  
+  multiplicacao = lambda numeroPrimeiro, numeroSegundo: print("Multiplicação:", numeroPrimeiro * numeroSegundo)
+multiplicacao(PrimeiroNumero, SegundoNumero)
